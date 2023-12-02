113a568036ee9833d540a0e100dd5db8da2805e9
muddyrain <qiu3291002845@gmail.com>
2023-12-02 21:18:04 +0800

    feat(article): 添加封面以及 tag标签

--- src/libs/article/article.entity.ts
+++ src/libs/article/article.entity.ts
@@ -2,13 +2,23 @@ import { CommonEntity } from '@/common/commonEntity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../user/user.entity';
 
+enum ArticleTag {
+  '前端',
+  '后端',
+  '移动端',
+  '数据库',
+  '服务器',
+  '人工智能',
+  '开发工具',
+  '代码人生',
+}
 @Entity()
 export class Article extends CommonEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({
-    nullable: true,
+    nullable: false,
     comment: '文章标题',
   })
   title: string;
@@ -16,15 +26,29 @@ export class Article extends CommonEntity {
   @Column({
     nullable: true,
     comment: '文章主题',
+    default: 'juejin',
   })
   theme: string;
 
   @Column({
     type: 'text',
-    nullable: true,
+    nullable: false,
     comment: '文章内容',
   })
   content: string;
+
+  @Column({
+    comment: '文章标签',
+    type: 'int',
+    nullable: false,
+  })
+  tag: ArticleTag;
+
+  @Column({
+    nullable: true,
+    comment: '文章封面',
+  })
+  coverPath: string;
 
   @ManyToOne(() => User, (user) => user.articles)
   user: User;
