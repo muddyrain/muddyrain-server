3b1364d74224c216457627e1c41427dd958a2bfc
muddyrain <qiu3291002845@gmail.com>
2023-12-17 19:54:45 +0800

    feat(recent-activity): 该路由鉴权公开放行

--- src/libs/recent-activity/recent-activity.controller.ts
+++ src/libs/recent-activity/recent-activity.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { RecentActivityService } from './recent-activity.service';
+import { AuthPublicMeta } from '@/decorators/AuthPublicMeta.decorator';
 
 @Controller('recent-activity')
 export class RecentActivityController {
   constructor(private readonly recentActivityService: RecentActivityService) {}
 
   @Get()
+  @AuthPublicMeta()
   findAll() {
     return this.recentActivityService.findAll();
   }
